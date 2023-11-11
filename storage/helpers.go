46a6e2f1576549b8fec0bfb88f5cb40cc17b5916
Riccardo Berto <riccardobrt@gmail.com>
2023-11-11 18:22:34 +0100

    removed unused functions

--- storage/helpers.go
+++ storage/helpers.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type dbOperation struct {
@@ -59,53 +58,4 @@ func (dbOp *dbOperation) chainDBOperation() bool {
 
 func (dbOp *dbOperation) unchainDBOperation() {
 	dbOp.ChainOp = false
-}
-
-func (dbOp *dbOperation) dummyWrite() error {
-	if dbOp.Txn == nil {
-		return errors.New("dbOp.Txn is nil")
-	}
-
-	if !dbOp.WritePool {
-		return errors.New("dbOp.WritePool is false")
-	}
-
-	if _, err := dbOp.Txn.Exec("update redis_type set type = type where type = 's'"); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func insert[T any](a []T, index int, value T) []T {
-	n := len(a)
-	if index < 0 {
-		index = (index%n + n) % n
-	}
-	switch {
-	case index == n: // nil or empty slice or after last element
-		return append(a, value)
-
-	case index < n: // index < len(a)
-		a = append(a[:index+1], a[index:]...)
-		a[index] = value
-		return a
-
-	case index < cap(a): // index > len(a)
-		a = a[:index+1]
-		var zero T
-		for i := n; i < index; i++ {
-			a[i] = zero
-		}
-		a[index] = value
-		return a
-
-	default:
-		b := make([]T, index+1) // malloc
-		if n > 0 {
-			copy(b, a)
-		}
-		b[index] = value
-		return b
-	}
 }
